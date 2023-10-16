6b49981afacb90430eeccc6b26d5ee25921767ca
wanderFl <147624435+wanderFl@users.noreply.github.com>
2023-10-16 08:46:37 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wanderley_Flores_My_Project_WF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
